7326cd2afbc3009c049d0ee798800b67323f8275
josue.dosidiana <josue.dosidiana@gmail.com>
2024-08-30 15:23:39 +0100

    por motivos de teste limpei a class main

--- src/atm/BancATM.java
+++ src/atm/BancATM.java
@@ -1,8 +1,5 @@
 package atm;
 
-import banco.cliente.Cliente;
-import banco.conta.Conta;
-
 public abstract class BancATM implements IATM {
 
    
--- src/testes/Main.java
+++ src/testes/Main.java
@@ -1,8 +1,5 @@
 package testes;
 
-import banco.cliente.Cliente;
-import banco.conta.*;
-
 public class Main {
 
 	public static void main(String[] args) {
