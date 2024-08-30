a45c6dbb58fbb166124d9fd45b83101522c3fd97
josue.dosidiana <josue.dosidiana@gmail.com>
2024-08-30 19:37:46 +0100

    update:pronto para enfrentar o temido merge conflict.

--- src/atm/BancATM.java
+++ src/atm/BancATM.java
@@ -1,6 +1,33 @@
 package atm;
 
-public abstract class BancATM {
+import banco.conta.Conta;
+
+public class BancATM extends ATM {
+
+    @Override
+    protected void consultar() {
+     
+    }
+
+    @Override
+    protected void levantamento(double importancia) {
+       
+    }
+
+    @Override
+    protected void transferir(Conta contaDestino, double importancia) {
+       
+    }
+
+    @Override
+    protected void efectuarPagamento(String servico, double montante) {
+        
+    }
+
+    @Override
+    protected void efectuarCompra(String item, double montante) {
+       
+    }
 
    
 }
